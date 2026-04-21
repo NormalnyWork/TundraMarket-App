f4642e5bef57736ae584d268dd6287cdcdaaccf5
4nk1r <4nk1r.dev@gmail.com>
2026-04-21 11:23:28 +0500

    Libraries setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,26 +4,23 @@ plugins {
 }
 
 android {
-    namespace = "com.normalnywork.tundramarket"
+    namespace = Config.PACKAGE_NAME
+
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(Config.COMPILE_SDK)
     }
 
     defaultConfig {
-        applicationId = "com.normalnywork.tundramarket"
-        minSdk = 24
-        targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        applicationId = Config.PACKAGE_NAME
+        minSdk = Config.MIN_SDK
+        targetSdk = Config.TARGET_SDK
+        versionCode = Config.VERSION_CODE
+        versionName = Config.VERSION_NAME
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -40,19 +37,13 @@ android {
 }
 
 dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.lifecycle.runtime.ktx)
+    // Jetpack Compose
     implementation(libs.androidx.activity.compose)
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.compose.ui.graphics)
-    implementation(libs.androidx.compose.ui.tooling.preview)
-    implementation(libs.androidx.compose.material3)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-    androidTestImplementation(platform(libs.androidx.compose.bom))
-    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
-    debugImplementation(libs.androidx.compose.ui.tooling)
-    debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation(platform(libs.compose.bom))
+    implementation(libs.bundles.compose)
+    debugImplementation(libs.compose.ui.tooling)
+    // DI
+    implementation(libs.bundles.koin)
+    // Navigation
+    implementation(libs.bundles.decompose)
 }
---
+++ buildSrc/src/main/kotlin/Config.kt
@@ -0,0 +1,11 @@
+object Config {
+
+    const val VERSION_CODE = 1
+    const val VERSION_NAME = "1.0"
+
+    const val MIN_SDK = 24
+    const val TARGET_SDK = 37
+    const val COMPILE_SDK = 37
+
+    const val PACKAGE_NAME = "com.normalnywork.tundramarket"
+}
